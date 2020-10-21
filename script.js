f9614df6242683258ae2ff8f699175ac300cb4e2
ramanan <nnraam@hotmail.com>
2020-10-21 16:02:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// deel a
+
+/*const ikRockArrowFunctions = () =>
+    console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();*/
+// deel b
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+//deel c
+const myFunction = (a, b) => a + b;
+//deel d
+const myFunction = (a) => a + 5;
+//deel e
+const createObject = () => ({ value: "hoi" });
